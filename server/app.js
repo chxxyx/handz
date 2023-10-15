8aaad23a0601a7142e780231ac322ac7ef492511
chxxyx <codud0826@naver.com>
2023-10-16 03:23:21 +0900

    [Chore] app.js 경로 설정 추가 및 gitignore 수정

--- server/app.js
+++ server/app.js
@@ -2,6 +2,7 @@ import express from 'express';
 import cors from 'cors';
 import path from 'path'; 
 import db from './database/index.js';
+import { routes } from './routes/index.js';
 
 const app = express();
 const PORT = process.env.PORT || 8000;
@@ -10,6 +11,15 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cors());
 
+app.use(express.static(path.join(new URL('../client/build', import.meta.url).pathname)));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(new URL('../client/build/index.html', import.meta.url).pathname));
+  });
+
+routes.forEach((route) => {
+    app.use('/', route);
+});
 
 db.sequelize.sync({ force: false }).then(() => {
     app.listen(PORT, () => {
