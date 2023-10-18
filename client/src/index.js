868d5068e5388704a3efcca9ce23b7c33d04d79f
chxxyx <codud0826@naver.com>
2023-10-18 21:33:23 +0900

    [Add] 배포 테스트 1

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { AuthProvider } from './AuthContext';
 import App from './App';
 import "./style/App.css"
 
@@ -9,8 +8,6 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 // createRoot로 생성한 root를 사용하여 앱을 렌더링합니다.
 root.render(
   <React.StrictMode>
-    {/* <AuthProvider> */}
       <App />
-    {/* </AuthProvider> */}
   </React.StrictMode>
 );
