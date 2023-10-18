7e8c3a19f327ebde6fae09bc8809a0ec8554332f
chxxyx <codud0826@naver.com>
2023-10-18 21:14:07 +0900

    [Delete] 백엔드 폴더 삭제

--- server/routes/index.js
+++
@@ -1,3 +0,0 @@
-import userRouter from './user.router.js';
-
-export const routes = [userRouter];
--- server/routes/user.router.js
+++
@@ -1,7 +0,0 @@
-import express from 'express';
-import { signUp } from '../controller/user.controller.js';
-
-const router = express.Router();
-
-router.route('/signup').post(signUp);
-export default router;
